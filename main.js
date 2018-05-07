720cfb0f5d1214baf25950f026d65bfe24b83d92
Daosheng Mu <daoshengmu@gmail.com>
2018-05-07 22:50:34 +0800

    project initializarion

---
+++ main.js
@@ -0,0 +1,90 @@
+
+function saveWithout401k(deposit, years) {
+  const tax = 0.28 + 0.093; // federal + state rate married
+  const rate = 1.04; // compound interest rate
+
+  let total = 0.0;
+
+  for (let i = 0; i < years; ++i) {
+    total = total * rate + deposit * rate * (1 - tax); 
+  }
+
+  return total;
+}
+
+function employerContribution(salary, percent) {
+
+}
+
+function depositBy401k(deposit, years) {
+  const rate = 1.04; // compound interest rate
+
+  let total = 0.0;
+  for (let i = 0; i < years; ++i) {
+    total = total * rate + deposit * rate; 
+  }
+
+  return total;
+}
+
+function calculate(){
+  let deposit = parseFloat(document.getElementById("deposit").value);
+  let years = parseFloat(document.getElementById("years").value);
+  let salary = parseFloat(document.getElementById("salary").value);
+  let employer = parseFloat(document.getElementById("employer").value) * 0.01;
+
+  console.log("deposit: " + deposit);
+  console.log("years: " + years);
+  console.log("salary: " + salary);
+  console.log("employer: " + employer);
+
+  const penalty = 0.1; // 401k penalty if you withdraw before you are 59.
+
+  let bySelf = depositBy401k(deposit, years);
+  let byEmployer = depositBy401k(salary * employer, years);
+
+  let output = document.getElementById("output");
+  let t = bySelf + byEmployer;
+  let selfResult = 0.0;
+  let n = 0;
+  const withdrawPerYear = 18650; // 10% federal rate for married
+  const tax = 0.1;    // 10% federal rate for married.
+  const rate = 1.04; // compound interest rate
+
+  // After penalty, tax.
+  // Withdraw in n years.
+  while (t > 0) {
+    selfResult += withdrawPerYear * (1 - penalty) * (1 - tax);
+    t -= withdrawPerYear;
+    t = rate * t;
+    ++n;
+  }
+
+  let t1 = byEmployer;
+  let n1 = 0;
+  let employerResult = 0.0;
+
+  // After penalty, tax.
+  // Withdraw in n years.
+  while (t1 > 0) {
+    employerResult += withdrawPerYear * (1 - penalty) * (1 - tax);
+    t1 -= withdrawPerYear;
+    t1 = rate * t1;
+    ++n1;
+  }
+  let with401k = selfResult + employerResult;
+
+  let without401k = saveWithout401k(deposit, years);
+  without401k += employerResult;
+
+  output.value = "Self Deposit: " + bySelf.toFixed(2).toString() + "\n" +
+                 "Employer Deposit: " + byEmployer.toFixed(2).toString() + "\n" +
+                 "Withdraw 401k after leaving US: " + n.toString() + " yrs, having " + with401k.toFixed(2).toString() + "\n" +
+                 "Without 401k after leaving US: " + n1.toString() + " yrs, having " +  without401k.toFixed(2).toString() + "\n" +
+                 "401K Earn: " + (with401k - without401k).toFixed(2).toString();
+}
+
+window.onload = function() {
+  let okBtn = document.getElementById("okBtn");
+  okBtn.addEventListener('click', calculate);
+}
